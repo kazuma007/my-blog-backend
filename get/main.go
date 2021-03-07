04d0c88945a20362405c0cc992e84b0329fa6432
kazuma007 <28896927+kazuma007@users.noreply.github.com>
2021-03-07 12:46:12 +0900

    initial commit

---
+++ get/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/kelseyhightower/envconfig"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+type Env struct {
+	S3AK string
+	S3SK string
+}
+
+type ResponseData struct {
+	Key            string `json:"storage_key", dynamodbav:"storage_key"`
+	Title          string `json:"title", dynamodbav:"title"`
+	Content        string `json:"content", dynamodbav:"content"`
+	RegisteredTime string `json:"registered_time", dynamodbav:"registered_time"`
+}
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context) (Response, error) {
+	var env Env
+	if err := envconfig.Process("", &env); err != nil {
+		fmt.Println("envconfig error", err)
+		return Response{StatusCode: 500}, err
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(env.S3AK, env.S3SK, ""),
+		Region:      aws.String("ap-northeast-1"),
+	}))
+
+	svc := dynamodb.New(sess)
+
+	params := &dynamodb.ScanInput{
+		TableName: aws.String("my-blog-t"),
+		Limit:     aws.Int64(50),
+	}
+
+	result, err := svc.Scan(params)
+	if err != nil {
+		fmt.Println("dynamoDB scan error", err)
+		return Response{StatusCode: 500}, err
+	}
+
+	var responseData []ResponseData
+	dynamodbattribute.UnmarshalListOfMaps(result.Items, &responseData)
+
+	jsonResponse, err := json.Marshal(responseData)
+	if err != nil {
+		fmt.Println("json marshal error", err)
+		return Response{StatusCode: 500}, err
+	}
+
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            string(jsonResponse),
+		Headers: map[string]string{
+			"Content-Type":           "application/json",
+			"X-MyCompany-Func-Reply": "hello-handler",
+		},
+	}
+
+	return resp, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ post/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/google/uuid"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Env struct {
+	S3AK string
+	S3SK string
+}
+
+type RequestBody struct {
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	File      string `json:"file"`
+	Extension string `json:"extension"`
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	requestBody := new(RequestBody)
+	if err := json.Unmarshal(([]byte)(request.Body), requestBody); err != nil {
+		fmt.Println("json unmarshal error", err)
+	}
+
+	uuidV4 := uuid.New()
+	createdId := uuidV4.String()
+
+	var env Env
+	if err := envconfig.Process("", &env); err != nil {
+		fmt.Println("envconfig error", err)
+		return events.APIGatewayProxyResponse{}, nil
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(env.S3AK, env.S3SK, ""),
+		Region:      aws.String("ap-northeast-1"),
+	}))
+
+	if err := updateDynamoDB(requestBody, createdId, sess); err != nil {
+		fmt.Println("updateDynamoDB error", err)
+		return events.APIGatewayProxyResponse{}, nil
+	}
+
+	if len(requestBody.File) != 0 {
+		if err := updateS3(requestBody, createdId, sess); err != nil {
+			fmt.Println("updateS3 error", err)
+			return events.APIGatewayProxyResponse{}, nil
+		}
+	}
+
+	return events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Headers": "origin,Accept,Authorization,Content-Type",
+			"Content-Type":                 "application/json",
+		},
+		Body:       "OK",
+		StatusCode: 200,
+	}, nil
+}
+
+func updateDynamoDB(requestBody *RequestBody, createdId string, sess *session.Session) error {
+	svc := dynamodb.New(sess)
+
+	slice := strings.Split(requestBody.Extension, "/")
+
+	time.Local = time.FixedZone("Asia/Tokyo", 9*60*60)
+	time.LoadLocation("Asia/Tokyo")
+	t := time.Now()
+	layout := "2006/01/02T15:04:05"
+	registeredTime := t.Format(layout)
+
+	putParams := &dynamodb.PutItemInput{
+		TableName: aws.String("my-blog-t"),
+		Item: map[string]*dynamodb.AttributeValue{
+			"storage_key": {
+				S: aws.String(createdId + "." + slice[1]),
+			},
+			"title": {
+				S: aws.String(requestBody.Title),
+			},
+			"content": {
+				S: aws.String(requestBody.Content),
+			},
+			"registered_time": {
+				S: aws.String(registeredTime),
+			},
+		},
+	}
+
+	if _, err := svc.PutItem(putParams); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateS3(requestBody *RequestBody, createdId string, sess *session.Session) error {
+	uploader := s3manager.NewUploader(sess)
+
+	data, err := base64.StdEncoding.DecodeString(requestBody.File[strings.IndexByte(requestBody.File, ',')+1:])
+
+	if err != nil {
+		return err
+	}
+
+	wb := new(bytes.Buffer)
+	wb.Write(data)
+
+	slice := strings.Split(requestBody.Extension, "/")
+
+	bucketName := "my-blog-storage"
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket:      aws.String(bucketName),
+		Key:         aws.String(createdId + "." + slice[1]),
+		Body:        wb,
+		ContentType: aws.String(requestBody.Extension),
+		ACL:         aws.String("public-read"),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
